f737eb9cf6d0a8d4f368312b6865bc9a145d089d
Terry Zhao <terry@runllama.ai>
2025-09-05 15:34:35 -0700

    inital commit

---
+++ client-sdk/src/index.ts
@@ -0,0 +1,13 @@
+/**
+ * @llamaindex/workflows-client
+ * 
+ * TypeScript client for LlamaIndex Workflows server.
+ * 
+ * @license MIT
+ */
+
+// Re-export everything from generated code
+export * from './generated';
+
+// Export version from package.json
+export { version as VERSION } from '../package.json';
