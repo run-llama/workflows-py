f53587fb3cea240a51133472df62cbb1c493f40f
Clelia (Astra) Bertelli <clelia@runllama.ai>
2025-09-10 13:09:53 +0200

    chore: make the test runner an async context manager again

--- src/workflows/testing/runner.py
+++ src/workflows/testing/runner.py
@@ -1,18 +1,67 @@
 from typing import AsyncContextManager, Any, Optional, TYPE_CHECKING
 from types import TracebackType
 from collections import Counter
+from dataclasses import dataclass
 
 if TYPE_CHECKING:
     from workflows import Workflow
 from workflows.events import StartEvent, Event, EventType
 
 
-class WorkflowTestRunner(AsyncContextManager["WorkflowTestRunner"]):
-    def __init__(self, workflow: "Workflow"):
-        self._workflow = workflow
+@dataclass
+class WorkflowTestResult:
+    """Container for workflow test results"""
 
-    async def __aenter__(self) -> Any:
+    collected: list[Event]
+    event_types: dict[EventType, int]
+    result: Any
+
+
+class WorkflowTestRunner(AsyncContextManager["WorkflowTestRunner"]):
+    def __init__(
+        self,
+        workflow: "Workflow",
+        start_event: StartEvent,
+        expose_internal: bool = True,
+        exclude_events: Optional[list[EventType]] = None,
+    ):
+        """
+        Run a workflow end-to-end and collect the events that are streamed during its execution.
+
+        Args:
+            start_event (StartEvent): The input event for the workflow
+            expose_internal (bool): Whether or not to expose internal events. Defaults to True if not set.
+            exclude_events. (list[EventType]): A list of event types to exclude from the collected events. Defaults to None if not set.
+
+        Returns:
+            WorkflowTestResult
+
+        Example:
+            ```
+            wf = GreetingWorkflow()
+            runner = WorkflowTestRunner(wf)
+
+            async with runner as test_wf:
+                if test_wf.result:
+                    assert test_wf.result.collected == 22
+                    assert test_result.result.event_types.get(StepStateChanged, 0) == 8
+                    assert str(test_result.result.result) == "hello Adam!"
+            ```
+        """
+        self._workflow = workflow
+        self._start_event = start_event
+        self._expose_internal = expose_internal
+        self._exclude_events = exclude_events
+        self._handler = None
+        self._result: Optional[WorkflowTestResult] = None
+
+    async def __aenter__(self) -> "WorkflowTestRunner":
+        await self._run()
         return self
+
+    @property
+    def result(self) -> Optional[WorkflowTestResult]:
+        return self._result
 
     async def __aexit__(
         self,
@@ -23,42 +72,17 @@ class WorkflowTestRunner(AsyncContextManager["WorkflowTestRunner"]):
         """Called when exiting the 'async with' block"""
         return not exc_type and not exc_val and not exc_tb
 
-    async def run(
-        self,
-        start_event: StartEvent,
-        expose_internal: bool = True,
-        exclude_events: Optional[list[EventType]] = None,
-    ) -> tuple[list[Event], dict[EventType, int], Any]:
-        """
-        Run a workflow end-to-end and collect the events that are streamed during its execution.
-
-        Args:
-            start_event (StartEvent): The input event for the workflow
-            expose_internal (bool): Whether or not to expose internal events. Defaults to True if not set.
-            exclude_events. (list[EventType]): A list of event types to exclude from the collected events. Defaults to None if not set.
-
-        Returns:
-            A tuple containing the list of collected events, a dictionary mapping each event type with its count in the collected events and the result of the workflow run.
-
-        Example:
-            ```
-            wf = GreetingWorkflow()
-
-            async with WorkflowTestRunner(wf) as test_runner:
-                collected, ev_types, result = await test_runner.run(start_event=StartEvent(name="Adam", greeting="hello"))
-                assert len(collected) == 22
-                assert ev_types.get(StepStateChanged, 0) == 8
-                assert str(result) == "hello Adam!"
-            ```
-        """
-        handler = self._workflow.run(start_event=start_event)
+    async def _run(self) -> None:
+        handler = self._workflow.run(start_event=self._start_event)
         collected_events: list[Event] = []
-        async for event in handler.stream_events(expose_internal=expose_internal):
-            if exclude_events and type(event) in exclude_events:
+        async for event in handler.stream_events(expose_internal=self._expose_internal):
+            if self._exclude_events and type(event) in self._exclude_events:
                 continue
             collected_events.append(event)
         result = await handler
         event_freqs: dict[EventType, int] = dict(
             Counter([type(ev) for ev in collected_events])
         )
-        return collected_events, event_freqs, result
+        self._result = WorkflowTestResult(
+            collected=collected_events, result=result, event_types=event_freqs
+        )
--- tests/test_testing_utils.py
+++ tests/test_testing_utils.py
@@ -9,6 +9,7 @@ from workflows.events import (
 )
 from workflows import Context, Workflow, step
 from workflows.testing import WorkflowTestRunner
+from workflows.testing.runner import WorkflowTestResult
 
 
 class SecondEvent(Event):
@@ -33,17 +34,25 @@ class SimpleWf(Workflow):
 @pytest.mark.asyncio
 async def test_testing_utils() -> None:
     wf = SimpleWf()
-    async with WorkflowTestRunner(wf) as test_runner:
-        assert isinstance(test_runner, WorkflowTestRunner)
-        collected, freqs, res = await test_runner.run(
-            start_event=StartEvent(message="hi"),  # type: ignore
-            exclude_events=[EventsQueueChanged],
+    runner = WorkflowTestRunner(
+        wf,
+        start_event=StartEvent(message="hi"),  # type: ignore
+        exclude_events=[EventsQueueChanged],
+    )
+    async with runner as wf_test_run:
+        assert isinstance(wf_test_run, WorkflowTestRunner)
+        assert wf_test_run.result is not None
+        assert isinstance(wf_test_run.result, WorkflowTestResult)
+        assert len(wf_test_run.result.collected) == sum(
+            [wf_test_run.result.event_types[k] for k in wf_test_run.result.event_types]
         )
-        assert len(collected) == sum([freqs[k] for k in freqs])
-        assert freqs.get(EventsQueueChanged, 0) == 0
-        assert freqs.get(SecondEvent, 0) == 1
-        assert freqs.get(StopEvent, 0) == 1
-        assert freqs.get(StepStateChanged, 0) == len(collected) - (
-            freqs.get(SecondEvent, 0) + freqs.get(StopEvent, 0)
+        assert wf_test_run.result.event_types.get(EventsQueueChanged, 0) == 0
+        assert wf_test_run.result.event_types.get(SecondEvent, 0) == 1
+        assert wf_test_run.result.event_types.get(StopEvent, 0) == 1
+        assert wf_test_run.result.event_types.get(StepStateChanged, 0) == len(
+            wf_test_run.result.collected
+        ) - (
+            wf_test_run.result.event_types.get(SecondEvent, 0)
+            + wf_test_run.result.event_types.get(StopEvent, 0)
         )
-        assert str(res) == "done"
+        assert str(wf_test_run.result.result) == "done"
